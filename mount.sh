d3357a8070feb1b292f16b0b243afb2f1dd25ea3
Tatsuo Nagamatsu <tatsuo.nagamatsu@sony.com>
2018-02-28 13:12:22 +0900

    add comment

--- mount.sh
+++ mount.sh
@@ -7,18 +7,24 @@ BNAME=$(date "+%Y%m%d%H%M")
 #apt-get install -y git-lfs
 #git lfs install
 
+echo "git clone"
 git clone https://github.com/TE-tatuonagamatu/loopback.git
 
+echo "mount image"
 if [ ! -d /mnt ]; then
     mkdir /mnt
 fi
 LOOPDEV=$(losetup -f)
 losetup "${LOOPDEV}" loopback/Ubuntu_armhf_nopkg.img
 mount "${LOOPDEV}" /mnt
+echo "update image"
 touch /mnt/"${BNAME}"
+echo "unmount"
 umount /mnt
 losetup -d "${LOOPDEV}"
 
+echo "push to server"
+cd loopback
 git checkout -b "${BNAME}"
 git add .
 git push -u origin master
