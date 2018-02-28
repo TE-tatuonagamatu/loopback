c4b2b945b92cd4d3b4e30b0f46bc9d3218462941
Tatsuo Nagamatsu <tatsuo.nagamatsu@sony.com>
2018-02-28 12:50:57 +0900

    add curl

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ FROM ubuntu:16.04
 MAINTAINER Tatsuo Nagamatsu <nagamatu@gmail.com>
 
 RUN apt-get update
+RUN apt-get install -y curl
 RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 RUN apt-get install -y git-lfs
 RUN git lfs install
--- mount.sh
+++ mount.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 # Install git-lfs
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-apt-get install -y git-lfs
-git lfs install
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+#apt-get install -y git-lfs
+#git lfs install
+
+git clone https://github.com/TE-tatuonagamatu/loopback.git
 
 if [ ! -d /mnt ]; then
     mkdir /mnt
 fi
 LOOPDEV=$(losetup -f)
-losetup "${LOOPDEV}" Ubuntu_armhf_nopkg.img
+losetup "${LOOPDEV}" loopback/Ubuntu_armhf_nopkg.img
 mount "${LOOPDEV}" /mnt
 df
 ls -Rl /mnt
 umount /mnt
 losetup -d "${LOOPDEV}"
 dmesg | tail
-od -X Ubuntu_armhf_nopkg.img | head
+od -X loopback/Ubuntu_armhf_nopkg.img | head
