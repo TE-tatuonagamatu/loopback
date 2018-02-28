95d5cf3d22f5b9dc342268a47e6d4795da6f3e25
Tatsuo Nagamatsu <tatuo.nagamatu@jp.sony.com>
2018-02-28 12:25:04 +0900

    add git-lfs install

--- mount.sh
+++ mount.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# Install git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+apt-get install -y git-lfs
+git lfs install
+
 if [ ! -d /mnt ]; then
     mkdir /mnt
 fi
